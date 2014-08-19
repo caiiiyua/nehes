d57995c89d890f8ee1663431e1a2c2122ffcbcab
Yuanqing Cai <ckjacket@gmail.com>
2014-08-20 00:22:56 +0800

    start server

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-'use strict';
+// init
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+var morgan = require("morgan");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
 
-// Requires meanio
-var mean = require('meanio');
+// configuration
+mongoose.connect("mongodb://nehes:ck8568@ds033459.mongolab.com:33459/nehesdb");
 
-// Creates and serves mean application
-mean.serve({ /*options placeholder*/ }, function(app, config) {
-  console.log('Mean app started on port ' + config.port + ' (' + process.env.NODE_ENV + ')');
-});
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type:'application/vnd.apijson' }));
+app.use(methodOverride());
+
+// listen as server
+app.listen(8080);
+console.log("App listening on port 8080");
