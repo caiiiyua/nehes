9795e814f1e96ea9890e8fecdcd38eb943ccad1f
Yuanqing Cai <ckjacket@gmail.com>
2014-08-20 00:58:24 +0800

    server routes

--- server.js
+++ server.js
@@ -16,6 +16,58 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type:'application/vnd.apijson' }));
 app.use(methodOverride());
 
+// define model
+var Todo = mongoose.model('Todo', {
+    text : String
+});
+
+// routes
+// node apis
+// get all todos
+app.get('/api/todos', function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err) {
+            res.send(err);
+        };
+        res.json(todos);
+    });
+});
+
+// create todo and send back all todos after creation
+app.post('/api/todos', function(req, res) {
+    Todo.create({
+        text : req.body.text,
+        done : false
+    }, function(err, todo) {
+        if (err) {
+            res.send(err);
+        }
+        Todo.find(function(err, todos) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(todos);
+        });
+    });
+});
+
+// delete a todo
+app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err) {
+            res.send(err);
+        }
+        Todo.find(function(err, todos) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(todos);
+        });
+    });
+});
+
 // listen as server
 app.listen(8080);
 console.log("App listening on port 8080");
